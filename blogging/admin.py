fbe954dbb73db6867f5b8a2165b34996bf027662
Alex Bogdanov <abogdanov@gmail.com>
2021-03-13 15:39:03 -0800

    Added tests and blogging

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# a new import
+from blogging.models import Post
+
+# and a new admin registration
+admin.site.register(Post)
--- blogging/tests.py
+++ blogging/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+class PostTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+
+from blogging.models import Post  # add this import at the top
+
+class PostTestCase(TestCase):
+    # ...
+
+    # add this test method to the PostTestCase
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
