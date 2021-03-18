3a68d0e2911daf368ee64a04d4eb837582e8c8be
Alex Bogdanov <abogdanov@gmail.com>
2021-03-18 14:32:23 -0700

    Lab 7: Add categories to post form at admin interface

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Sample blog on Django with polling
 - unit tests
 
 
+Your Assignment
+You’ll be reversing that relationship so that you can only add categories to posts
+
+Take the following steps:
+
+- Read the documentation again about the Django admin.
+
+- You’ll need to create a customized ModelAdmin class for the Post and Category models.
+
+- And you’ll need to create an InlineModelAdmin to represent Categories on the Post admin view.
+
+- Finally, you’ll need to exclude the ‘posts’ field from the form in your Category admin.
--- blogging/admin.py
+++ blogging/admin.py
@@ -3,6 +3,31 @@ from django.contrib import admin
 # a new import
 from blogging.models import Post, Category
 
+
+class CategoryInline(admin.TabularInline):
+    """
+    Inline model to be attachd to posts form.
+
+    Note, because of  many-to-many it has 'through'
+    """
+    model = Category.posts.through
+
+
+class PostAdmin (admin.ModelAdmin):
+    """
+    Custom ModelAdmin  form for Post with inlined categories
+    """
+    inlines = [CategoryInline,]
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Custom ModelAdmin for Categories form w/ hidden posts field
+    """
+    fields = ('name', 'description')
+    pass
+
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
